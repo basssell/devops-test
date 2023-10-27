d9c4548ae16029549a1ea3e8b388c346d5c856e9
Wassim <rachedwassimbassel@gmail.com>
2023-10-27 19:55:12 +0100

    ya bismillah

---
+++ src/main/java/com/example/devopsdebut/DevopsDebutApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsdebut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsDebutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsDebutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopsdebut/DevopsDebutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsdebut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsDebutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
